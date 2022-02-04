b132a406c5c45b4a3df8d6cf49eed4d16a131a7b
annaz0506 <annaz0506@icloud.com>
2022-02-04 12:27:38 -0800

    Fixed Test

--- SkillsDemoTester.java
+++ SkillsDemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillsDemoTester {
     @Test
     public void multiply() {
-        assertEquals(3, SkillsDemo.multiply(2,3)); //fails initially
+        assertEquals(3, SkillsDemo.multiply(1,3)); //fails initially
     }
 }
